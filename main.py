cd2c9df8922245b9b94ec4f86b87d0fac480b16a
jianis17 <84100526+jianis17@users.noreply.github.com>
2022-03-08 16:29:11 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+form  random import randrange
+n = randrange(1000)
+while True:
+    v = int(input())
+    if n == v:
+        print('You win')
+        break
+        print('Smaller ' if (n < v) else 'Bigger' )
